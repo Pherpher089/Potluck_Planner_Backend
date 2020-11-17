21d8c1c576995d90c629c13acd2168c96fb065cd
Pherpher089 <Christutor089@gmail.com>
2020-11-16 23:54:54 -0800

    Adding access-control-access-orgin header to every route

--- api/server.js
+++ api/server.js
@@ -8,12 +8,15 @@ const potlucksRouter = require("./routes/potlucks-router.js");
 const foodRouter = require("./routes/food-router.js");
 
 const server = express();
+server.use(bodyParser({ extended: false }));
 const options = {
 	origin: 'https://potluck-planner-app.netlify.app',
 }
 server.use(cors(options));
 server.use(helmet());
-
+server.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+})
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
 server.use("/api/potlucks", potlucksRouter);
