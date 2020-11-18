3d4b801366b7d5a23919ae85d72194db32746357
Pherpher089 <Christutor089@gmail.com>
2020-11-18 00:10:14 -0800

    cors

--- api/server.js
+++ api/server.js
@@ -9,16 +9,16 @@ const foodRouter = require("./routes/food-router.js");
 
 const server = express();
 //server.use(bodyParser({ extended: false }));
-const options = {
-	origin: 'https://potluck-planner-app.netlify.app',
-}
-server.use(cors(options));
-server.use(helmet());
 server.use((req, res, next) => {
 	req.header('Access-Control-Allow-Origin: *');
 	req.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 	req.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 })
+const options = {
+	origin: 'https://potluck-planner-app.netlify.app',
+}
+server.use(cors(options));
+server.use(helmet());
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
 server.use("/api/potlucks", potlucksRouter);
