5968fe85a62e598ca2ab816030bbda2228c74587
Pherpher089 <Christutor089@gmail.com>
2020-12-02 23:29:05 -0800

    typo on server name

--- api/server.js
+++ api/server.js
@@ -7,8 +7,7 @@ const usersRouter = require("./routes/users-router.js");
 const potlucksRouter = require("./routes/potlucks-router.js");
 const foodRouter = require("./routes/food-router.js");
 
-const server = express();
-server.use(bodyParser({ extended: false }));
+
 var allowList = ['https://potluck-planner-app.netlify.app', 'http://localhost:3000/', '*']
 var corsOptionsDelegate = function (req, callback) {
 	var corsOptions = {
@@ -17,14 +16,16 @@ var corsOptionsDelegate = function (req, callback) {
 
 	callback(null, corsOptions) // callback expects two parameters: error and options
 }
-app.use(function (req, res, next) {
+const server = express();
+server.use(bodyParser({ extended: false }));
+server.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 
 server.use(cors(corsOptionsDelegate));
-//server.use(helmet());
+server.use(helmet());
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
 server.use("/api/potlucks", potlucksRouter);
