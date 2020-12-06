3568fe0770b81016da0bdc773780f68a2f6ff2e5
Pherpher089 <Christutor089@gmail.com>
2020-12-05 22:50:31 -0800

    More Cors Baby

--- api/server.js
+++ api/server.js
@@ -7,21 +7,6 @@ const usersRouter = require("./routes/users-router.js");
 const potlucksRouter = require("./routes/potlucks-router.js");
 const foodRouter = require("./routes/food-router.js");
 
-
-// var allowList = ['https://potluck-planner-app.netlify.app', 'http://localhost:3000/', '*']
-// var corsOptionsDelegate = function (req, callback) {
-// 	var corsOptions = {
-// 		origin: '*'
-// 	};
-
-// 	callback(null, corsOptions) // callback expects two parameters: error and options
-// }
-
-// server.use(function (req, res, next) {
-// 	res.header("Access-Control-Allow-Origin", "*");
-// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// 	next();
-// });
 var corsOptions = {
 	origin: '*',
 	allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept', 'auth'],
@@ -29,9 +14,9 @@ var corsOptions = {
 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 const server = express();
-server.use(cors(corsOptions));
 server.use(bodyParser({ extended: false }));
 server.use(helmet());
+server.use(cors(corsOptions));
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
 server.use("/api/potlucks", potlucksRouter);
